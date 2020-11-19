85f0308634715d08b89abf7204005b96f7279b17
tris0008 <triston_99_1997@hotmail.com>
2020-11-19 20:53:01 +0800

    student drop and print course

---
+++ Java Workspace/src/STARS_system/STARS_system/menu.java
@@ -0,0 +1,15 @@
+package STARS_system;
+import java.util.*;
+
+public class menu {
+	public static void main(String args[]) {
+		
+		menu c1 = new menu();
+		c1.display_student_menu();
+	}
+	
+	
+	public void display_student_menu() {
+		System.out.println("Hello");
+	}
+}
